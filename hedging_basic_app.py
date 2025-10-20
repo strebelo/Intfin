487f26981cc4c36c60b97945bd1d6c4566cd790d
strebelo <s.rebelo@me.com>
2025-10-19 19:43:08 -0500

    Fix formatting of bid-ask spread comment

--- hedging_basic_app.py
+++ hedging_basic_app.py
@@ -227,7 +227,7 @@ with st.expander("Show math / notation"):
     st.latex(r"\text{PV(profit)} = \sum_{t=1}^T \left(\text{revenue}_t^{(DC)}-\text{cost}_t^{(DC)}\right)\cdot DF_d(t)")
     st.markdown(
         r"""
-#**Bid–ask spread per tenor**: if input is \(s_{\text{per-year}}\) (bps/yr), a \(k\)-year forward uses  
+# **Bid–ask spread per tenor**: if input is \(s_{\text{per-year}}\) (bps/yr), a \(k\)-year forward uses  
 #\( s_{\text{total}} = k \cdot s_{\text{per-year}} \) bps.
 #        """
     )
