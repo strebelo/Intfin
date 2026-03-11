3f1091379423e894d36fb9d2d8bb99356f184eba
strebelo <s.rebelo@me.com>
2026-03-10 21:39:15 -0500

    Update Vintage.py

--- Vintage.py
+++ Vintage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, roc_auc_score
 
 st.title("Port Vintage Declaration Prediction")
